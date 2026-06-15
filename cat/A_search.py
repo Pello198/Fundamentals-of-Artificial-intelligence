b54fd6846fc3727cacbdf95cfb757f1be08718ff
pello198 <kwemoipello@gmail.com>
2026-06-15 20:23:15 +0300

    Refactor A* search algorithm
    Removed comments for clarity and improved code readability.

--- cat/A_search.py
+++ cat/A_search.py
@@ -1,6 +1,4 @@
 # A* Search Algorithm in Python
-
-# Graph represented as adjacency list
 graph = {
     'A': [('B', 1), ('C', 3)],
     'B': [('D', 3), ('E', 6)],
@@ -23,18 +21,10 @@ heuristic = {
 }
 
 def search (start, goal):
-
-    # Open list stores nodes to be explored
     open_list = [start]
-
-    # Closed list stores visited nodes
     closed_list = []
-
-    # Cost from start to node
     g = {}
     g[start] = 0
-
-    # Parent nodes for path reconstruction
     parent = {}
     parent[start] = start
 
@@ -61,12 +51,9 @@ def search (start, goal):
 
             print("Optimal Path:", path)
             return
-
-        # Move current node from open to closed
         open_list.remove(current)
         closed_list.append(current)
 
-        # Explore neighbors
         for neighbor, cost in graph[current]:
 
             if neighbor not in open_list and neighbor not in closed_list:
@@ -86,6 +73,4 @@ def search (start, goal):
                     if neighbor in closed_list:
                         closed_list.remove(neighbor)
                         open_list.append(neighbor)
-
-# Run algorithm
 search('A', 'G')
